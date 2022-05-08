c4c15bfa6fa22c1bf016c7ea5caa75c6624eaff2
mgdiana <49156411+mgdiana@users.noreply.github.com>
2022-05-08 13:17:05 +0200

    Weather forecast week Done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,145 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let suffix = "AM";
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (hours > 12) {
+    hours = hours - 12;
+    suffix = `PM`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes} ${suffix}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+            <div class="col-2">
+              <div class="weather-forecast-day">${formatDay(
+                forecastDay.dt
+              )}</div>
+            
+              <img src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="Clear" width="42"/>
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-max">${Math.round(
+                  forecastDay.temp.max
+                )}° </span>
+                <span class="weather-forecast-min">${Math.round(
+                  forecastDay.temp.min
+                )}° </span>
+            </div>
+          </div>
+    `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "e95d2dbcb61d3a71412ef6bae16796e5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  getForecast(response.data.coord);
+}
+
+function searchCity(city) {
+  let apiKey = "e95d2dbcb61d3a71412ef6bae16796e5";
+  let units = "metric";
+  let aUrl = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${aUrl}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let dateElement = document.querySelector("#current-date-time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-city");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Castelló d'Empúries");
+
+/*function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -20,7 +161,66 @@ function formatDate(date) {
   ];
   let day = days[dayIndex];
 
-  return `${day} ${hours}:${minutes}`;
+  return `${day}, ${hours}:${minutes}`;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}°</span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}°</span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "e95d2dbcb61d3a71412ef6bae16796e5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
@@ -41,6 +241,7 @@ function displayWeatherCondition(response) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
 }
+
 function searchCity(city) {
   let apiKey = "e95d2dbcb61d3a71412ef6bae16796e5";
   let units = "metric";
@@ -77,70 +278,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-city");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("Castelló d'Empúries");
-
-/*function searchYourCity(event) {
-  event.preventDefault();
-
-  let textInput = document.querySelector("#text-input");
-
-  let h1 = document.querySelector("h1");
-  if (textInput.value) {
-    h1.innerHTML = textInput.value;
-  } else {
-    h1.innerHTML = "Empuriabrava";
-  }
-  let cityWeather = `${apiURL}?q=~${h1.innerHTML}&appid=${apiKey}&units=${units}}`;
-  axios.get(cityWeather).then(searchYourCity);
-}
-
-function showCurrentWeather(response) {
-  let currentWeather = document.querySelector("#temperature");
-  let h1 = document.querySelector("h1");
-  currentWeather.innerHTML = Math.round(response.data.main.temp);
-  h1.innerHTML = response.data.name;
-}
-
-function currentPosition(event) {
-  function currentLocation(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    let cityApiUrl = `${apiURL}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-    axios.get(cityApiUrl).then(showCurrentWeather);
-  }
-  navigator.geolocation.getCurrentPosition(findCurrentLocation);
-}
-
-let now = new Date();
-let todayDayTime = document.querySelector("#currentDayTime");
-todayDayTime.innerHTML = getDayTime(now);
-
-let cityInputForm = document.querySelector("#text-form");
-cityInputForm.addEventListener("submit", currentPosition);
-let h1 = document.querySelector("h1");
-let cityWeather = `${apiURL}?q=${h1.innerHTML}&appid=${apiKey}&units=${units}`;
-
-axios.get(cityWeather).then(currentPosition);
-
-let currentCityButton = document.querySelector("#current-city");
-currentCityButton.addEventListener("click", currentLocation);*/
-
-/*let textForm = document.querySelector("#text-form");
-textForm.addEventListener("submit", searchYourCity);
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = 55;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = 13;
-}
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);*/
+searchCity("Castelló d'Empúries");*/
